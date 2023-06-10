d8448461e51f4abcffbee64fb1df3dbd5ab80489
chabitblunk <cblunk097@gmail.com>
2023-06-09 21:09:36 -0600

    Add todo complete functionality

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -13,17 +13,34 @@ const (
 )
 
 func main() {
+
   add := flag.Bool("add", false, "add a new todo")
+  complete := flag.Int("complete", 0, "mark a todo as complete")
+
   flag.Parse()
+
   todos := &todo.Todos{}
+
   if err := todos.Load(todoFile); err != nil {
     fmt.Fprintln(os.Stderr, err.Error())
     os.Exit(1)
   }
+
   switch {
   case *add:
     todos.Add("sample todo")
     err := todos.Store(todoFile)
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err.Error())
+      os.Exit(1)
+    }
+  case *complete > 0:
+    err := todos.Complete(*complete)
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err.Error())
+      os.Exit(1)
+    }
+    err = todos.Store(todoFile)
     if err != nil {
       fmt.Fprintln(os.Stderr, err.Error())
       os.Exit(1)
