ca6129a0f4ef0cd7e10772375792a0dd727b4614
chabitblunk <cblunk097@gmail.com>
2023-06-09 22:12:26 -0600

    Working tabulation and coloring

--- todo.go
+++ todo.go
@@ -3,9 +3,10 @@ package todo
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"github.com/alexeyco/simpletable"
 	"os"
 	"time"
+	"fmt"
 )
 
 type Item struct {
@@ -75,8 +76,51 @@ func (t *Todos) Store(filename string) error {
 }
 
 func (t *Todos) Print() {
-  for i, item := range *t {
-    i++
-    fmt.Printf("%d - %s\n", i, item.Task)
+  table := simpletable.New()
+  table.Header = &simpletable.Header{
+    Cells: []*simpletable.Cell {
+      {Align: simpletable.AlignCenter, Text: "#"},
+      {Align: simpletable.AlignCenter, Text: "Task"},
+      {Align: simpletable.AlignCenter, Text: "Done?"},
+      {Align: simpletable.AlignRight, Text: "CreatedAt"},
+      {Align: simpletable.AlignRight, Text: "CompletedAt"},
+    },
   }
+  var cells [][]*simpletable.Cell
+  
+  for idx, item := range *t {
+    idx++
+    task := blue(item.Task)
+    done := blue("no")
+    if item.Done {
+      task = green(fmt.Sprintf("\u2705 %s", item.Task))
+      done = green("yes")
+    }
+    cells = append(cells, []*simpletable.Cell{
+      {Text: fmt.Sprintf("%d", idx)},
+      {Text: task},
+      {Text: done},
+      {Text: item.CreatedAt.Format(time.RFC822)},
+      {Text: item.CompletedAt.Format(time.RFC822)},
+    })
+  }
+
+  table.Body = &simpletable.Body{Cells: cells}
+
+  table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+    {Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending tasks", t.CountPending()))},
+  }}
+
+  table.SetStyle(simpletable.StyleUnicode)
+  table.Println()
+}
+
+func (t *Todos) CountPending() (total int) {
+  total = 0
+  for _, item := range *t {
+    if !item.Done {
+      total++
+    }
+  }
+  return
 }
